a9841f79d13c977ad23f064e47fa513ab6e7236d
Kim Santiago <kisantia@microsoft.com>
2019-01-23 17:50:24 -0800

    upgrade plan is piped through and returns the xml plan

--- src/sql/sqlops.d.ts
+++ src/sql/sqlops.d.ts
@@ -1595,6 +1595,10 @@ declare module 'sqlops' {
 		operationId: string;
 	}
 
+	export interface UpgradePlanResult extends DacFxResult {
+		report: string;
+	}
+
 	export interface ExportParams {
 		databaseName: string;
 		packageFilePath: string;
@@ -1634,12 +1638,20 @@ declare module 'sqlops' {
 		taskExecutionMode: TaskExecutionMode;
 	}
 
+	export interface UpgradePlanParams {
+		packageFilePath: string;
+		databaseName: string;
+		ownerUri: string;
+		taskExecutionMode: TaskExecutionMode;
+	}
+
 	export interface DacFxServicesProvider extends DataProvider {
 		exportBacpac(databaseName: string, packageFilePath: string, ownerUri: string, taskExecutionMode: TaskExecutionMode): Thenable<DacFxResult>;
 		importBacpac(packageFilePath: string, databaseName: string, ownerUri: string, taskExecutionMode: TaskExecutionMode): Thenable<DacFxResult>;
 		extractDacpac(databaseName: string, packageFilePath: string, applicationName: string, applicationVersion: string, ownerUri: string, taskExecutionMode: TaskExecutionMode): Thenable<DacFxResult>;
 		deployDacpac(packageFilePath: string, databaseName: string, upgradeExisting: boolean, ownerUri: string, taskExecutionMode: TaskExecutionMode): Thenable<DacFxResult>;
 		generateDeployScript(packageFilePath: string, databaseName: string, scriptFilePath: string, ownerUri: string, taskExecutionMode: TaskExecutionMode): Thenable<DacFxResult>;
+		upgradePlan(packageFilePath: string, databaseName: string, ownerUri: string, taskExecutionMode: TaskExecutionMode): Thenable<UpgradePlanResult>;
 	}
 
 	// Security service interfaces ------------------------------------------------------------------------
